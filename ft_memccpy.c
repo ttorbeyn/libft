9680aa7c0fc16871159bd43092470391d471199d
ttorbeyn <ttorbeyn@shi-r3-p5.s19.be>
2021-01-08 14:49:43 +0100

    commit

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,9 +1,11 @@
+#include "includes/libft.h"
+
 void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while (i < n)
+	while ((i < n) && (((unsigned char*)src)[i] == c))
 	{
 		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
 		i++;
@@ -22,7 +24,7 @@ int main()
 
 	// On essaye de trouver la première phrase dans le texte.
 	char firstSentence[ length ];
-	char * res = memccpy( firstSentence, text, '.', length );
+	char * res = ft_memccpy( firstSentence, text, '.', length );
 
 	// On affiche le resultat.
 	if ( res != NULL ) {
