8c8e515ebd31c4670600f80ff97a9052f03edcca
ttorbeyn <ttorbeyn@shi-r3-p5.s19.be>
2021-01-08 18:55:36 +0100

    save libft.h

---
+++ libft.h
@@ -0,0 +1,29 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
+int 	ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void 	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void 	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t 	ft_strlen(const char *s);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+
+
+#endif
